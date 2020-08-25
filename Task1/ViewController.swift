05a092629029a7e2351f94daa4ae53ecd08dfa5e
TakumaOgawa <syuu.2531@gmail.com>
2020-08-25 20:52:53 +0900

    課題1完成

--- Task1/ViewController.swift
+++ Task1/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sumLabel: UILabel!
+    @IBOutlet var nums: [UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in 0..<nums.count {
+            nums[i].keyboardType = UIKeyboardType.numberPad
+        }
+
     }
 
+    @IBAction func sum(_ sender: Any) {
+        var sum = 0
+        for i in 0..<nums.count {
+            sum += Int(nums[i].text!) ?? 0
+        }
+        
+        sumLabel.text = String(sum)
+    }
 
 }
 
