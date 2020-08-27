8f9b0dc413c9e0405700f4c6884e8e2cf4f0e9f5
TakumaOgawa <syuu.2531@gmail.com>
2020-08-27 19:30:41 +0900

    forEachで書き直し。

--- Task1/ViewController.swift
+++ Task1/ViewController.swift
@@ -11,21 +11,37 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var sumLabel: UILabel!
-    @IBOutlet var nums: [UITextField]!
+//    @IBOutlet var nums: [UITextField]!
+    @IBOutlet var numberTextFields: [UITextField]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for i in 0..<nums.count {
-            nums[i].keyboardType = UIKeyboardType.numberPad
+//        for i in 0..<nums.count {
+//            nums[i].keyboardType = UIKeyboardType.numberPad
+//        }
+        
+        numberTextFields.forEach {
+            $0.keyboardType = .numberPad
         }
 
     }
 
     @IBAction func sum(_ sender: Any) {
         var sum = 0
-        for i in 0..<nums.count {
-            sum += Int(nums[i].text!) ?? 0
+//        for i in 0..<nums.count {
+//            sum += Int(nums[i].text!) ?? 0
+//        }
+        
+//        numberTextFields.forEach { (num) in
+//            sum += Int(num.text!) ?? 0
+//        }
+    
+        // $0はクロージャーの第一引数
+        // 上で言うnumが$0
+        
+        numberTextFields.forEach {
+            sum += Int($0.text!) ?? 0
         }
         
         sumLabel.text = String(sum)
